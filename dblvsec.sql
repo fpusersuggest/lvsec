4d1ceb9ea8f4a561c8a3feaef40813aa63fde3fe
fpusersuggest <mediomen27@gmail.com>
2018-02-20 02:15:56 +0100

    lvsec

--- dblvsec.sql
+++ dblvsec.sql
@@ -6,22 +6,35 @@
 PRAGMA foreign_keys = off;
 BEGIN TRANSACTION;
 
--- Table: t_offline
-DROP TABLE IF EXISTS t_offline;
-CREATE TABLE t_offline (
-	id 		INT PRIMARY KEY NOT NULL UNIQUE, 
-	nome 		TEXT NOT NULL, 
-	stato 	TEXT, 
-	sha256 	TEXT (64), 
-	md5 		TEXT (32), 
-	pkg 		INT REFERENCES t_pkg (id)
-);
 
--- Table: t_pkg
-DROP TABLE IF EXISTS t_pkg;
-CREATE TABLE t_pkg (
+-- Table: t_pkg_on
+DROP TABLE IF EXISTS t_pkg_on;
+CREATE TABLE t_pkg_on (
 	id 		INT PRIMARY KEY NOT NULL, 
 	pkg 		TEXT NOT NULL
+);
+
+-- Table: t_pkg_off
+DROP TABLE IF EXISTS t_pkg_off;
+CREATE TABLE t_pkg_off (
+	id 		INT PRIMARY KEY NOT NULL, 
+	pkg 		TEXT NOT NULL
+);
+
+-- Table: t_pkg_files_on
+DROP TABLE IF EXISTS t_pkg_files_on;
+CREATE TABLE t_pkg_files_on (
+	id 		INT PRIMARY KEY NOT NULL, 
+	file 		TEXT NOT NULL,
+	pkg		INT REFERENCES t_pkg (id)
+);
+
+-- Table: t_pkg_files_off
+DROP TABLE IF EXISTS t_pkg_files_off;
+CREATE TABLE t_pkg_files_off (
+	id 		INT PRIMARY KEY NOT NULL, 
+	file 		TEXT NOT NULL,
+	pkg		INT REFERENCES t_pkg (id)
 );
 
 -- Table: t_online
@@ -35,6 +48,28 @@ CREATE TABLE t_online (
 	pkg 		INT REFERENCES t_pkg (id)
 );
 
+
+-- Table: t_offline
+DROP TABLE IF EXISTS t_offline;
+CREATE TABLE t_offline (
+	id 		INT PRIMARY KEY NOT NULL UNIQUE, 
+	nome 		TEXT NOT NULL, 
+	stato 	TEXT, 
+	sha256 	TEXT (64), 
+	md5 		TEXT (32), 
+	pkg 		INT REFERENCES t_pkg (id)
+);
+
+
+-- Table: t_db
+DROP TABLE IF EXISTS t_db;
+CREATE TABLE t_db (
+	id 		INT PRIMARY KEY NOT NULL UNIQUE, 
+	desc		TEXT, 
+	data		date,
+	uuid		TEXT NOT NULL,
+	tipo		BOOL
+);
 
 
 COMMIT TRANSACTION;
