1560958defe7ecf963bff7c543d8d4ace22d545e
fpusersuggest <mediomen27@gmail.com>
2018-02-20 02:09:30 +0100

    lvsec

--- dblvsec.py
+++ dblvsec.py
@@ -2,65 +2,45 @@
 
 import os, sqlite3
 from tasklog import *
+from uuid import import uuid1
+
 #__name__="db"
 v_file_db="dblvsec.db"
 v_file_sch="dblvsec.sql"
 
 
-def f_conn(file):
-	"""Crea una connessione al database"""
-	try:
-		f_print("Connessione al db")
-		c=sqlite3.connect(file)
-		#print "stampo c = "+str(c)
-                if c is True:
-                    f_ok()
-                else:
-                    f_fail()
-	except:
-		f_fail()
+def f_conn(file2):
+    try:
+        c=sqlite3.connect(file2)
+    except:
+        print("Error")
 
 
-def create_table(conn, create_table_sql):
-	"""Crea una tabella"""
-	try:
-		c=conn.cursor()
-		c.execute(create_table_sql)
-	except Error as e:
-		print(e)
+def f_create_db(file1):
+    f_print("DB not found, I am going to create it")
+    conn=sqlite3.connect(v_file_db)
+    cur=conn.cursor()
+    f=open(file1, "r")
+    cur.executescript(f.read())
+    conn.commit()
+    f_ok()
 
 
 def main():
-	#m_conn = f_conn(v_file_db)
-	#print "stampo m_conn "+str(m_conn)
-        f_print("Try to connect to db")
-	if os.path.isfile(v_file_db) is not True:
-                f_fail()
-                f_print("Database not found, I create it")
-	        m_conn_create = f_conn(v_file_db)
-		m_schema=open(v_file_sch,"r").read()
-		m_cursore_create=m_conn_create.cursor()
-		m_ins_create=map(lambda x: x.lstrip().replace("\n"," "),m_schema.split(";"))
-		map(m_cursore_create.execute, m_ins)
-                if m_conn_create.commit() is True:
-                    f_ok()
-                else:
-                    f_fail()
-	else:
-		m_conn=f_conn(v_file_db)
-                if m_conn is True: 
-
-
-
+    f_ok()
+    #m_conn = f_conn(v_file_db)
+    #print "stampo m_conn "+str(m_conn)
+    f_print("Try to connect to db")
+    if os.path.isfile(v_file_db) is not True:
+        f_fail()
+        f_create_db(v_file_sch)
+    else:
+        m_conn=f_conn(v_file_db)
+        f_ok()
 
 
 if __name__ == '__main__':
     f_print("Let's start")
     main()
-
-
-
-
-
 
 
